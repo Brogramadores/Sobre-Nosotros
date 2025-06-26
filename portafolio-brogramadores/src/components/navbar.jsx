36a9e6617a822e91c16026f6975e2d5d8b91f449
Jose Esau Tovar Cruz <esautovarcz@gmail.com>
2025-06-26 11:46:47 -0600

    Navbar mejorado

--- portafolio-brogramadores/src/components/navbar.jsx
+++ portafolio-brogramadores/src/components/navbar.jsx
@@ -4,51 +4,64 @@ import img from '../img/img.jsx';
 import styles from '../Styles/Navbar.module.css';
 
 function Navbar() {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
-// ${styles.navbarCustom}
-    return (
-    <nav className={`${styles.navbarCustom}`}> 
-        <nav className={`navbar navbar-expand-lg`}>
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img src={img.Brogramadores} alt="Logo" width="85" height="55" className="d-inline-block align-text-top" />
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Inicio</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a 
-                                className="nav-link dropdown-toggle" 
-                                href="#" 
-                                role="button" 
-                                onClick={toggleDropdown}
-                                aria-expanded={isDropdownOpen}
-                            >
-                                Sobre Nosotros
-                            </a> 
-                            <ul className={`dropdown-menu ${isDropdownOpen ? 'show' : '' } `}>
-                                <li><a className="dropdown-item" href="#">Proyectos</a></li>
-                                <li><a className="dropdown-item" href="#">Redes Sociales</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Contactanos</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <nav className={`navbar navbar-expand-lg ${styles.navbarCustom}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src={img.Brogramadores}
+            alt="Logo"
+            width="85"
+            height="55"
+            className="d-inline-block align-text-top"
+          />
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Inicio</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                onClick={toggleDropdown}
+                aria-expanded={isDropdownOpen}
+              >
+                Sobre Nosotros
+              </a>
+              <ul className={`dropdown-menu ${isDropdownOpen ? 'show' : ''}`}>
+                <li><a className="dropdown-item" href="#">Proyectos</a></li>
+                <li><a className="dropdown-item" href="#">Redes Sociales</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Contáctanos</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    
-    );
+  );
 }
 
 export default Navbar;
