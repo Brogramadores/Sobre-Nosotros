3cbfd7fc77137b335a1de57b3caaf899af2bc37d
UP200190 <rapaelo.17.rmv@outlook.com>
2025-07-01 12:33:31 -0600

    Primeros Proyectos Beta

--- portafolio-brogramadores/src/components/Proyectos-Bro.jsx
+++ portafolio-brogramadores/src/components/Proyectos-Bro.jsx
@@ -1,67 +1,73 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../Styles/ProyectosBro.module.css'; // Crea este archivo para tu CSS adicional si lo requieres
+import styles from '../Styles/ProyectosBro.module.css';
+import proyecto1 from '../img/proyecto1.jpg';
+import proyecto2 from '../img/proyecto2.jpg';
+import proyecto3 from '../img/proyecto3.jpg';
+import proyecto4 from '../img/proyecto4.jpg';
+import proyecto5 from '../img/proyecto5.jpg';
+import proyecto6 from '../img/proyecto6.jpg';
 
 const ProyectosBro = () => {
   const projects = [
     {
       id: 1,
-      img: 'img/proyecto1.png',
+      img: proyecto1,
       title: 'Proyecto 1',
-      github: 'https://github.com',
+      github: 'https://github.com/Brogramadores',
       icon: 'bi-filetype-java',
     },
     {
       id: 2,
-      img: 'img/proyecto2.png',
+      img: proyecto2,
       title: 'Proyecto 2',
-      github: 'https://github.com',
+      github: 'https://github.com/Brogramadores',
       icon: 'bi-google-play',
     },
     {
       id: 3,
-      img: 'img/proyecto3.webp',
+      img: proyecto3,
       title: 'Proyecto 3',
-      github: 'https://github.com',
+      github: 'https://github.com/Brogramadores',
       icon: 'bi-hospital',
     },
     {
       id: 4,
-      img: 'img/proyecto4.png',
+      img: proyecto4,
       title: 'Proyecto 4',
-      github: 'https://github.com',
+      github: 'https://github.com/Brogramadores',
       icon: 'bi-file-excel-fill',
     },
     {
       id: 5,
-      img: 'img/proyecto5.jpg',
+      img: proyecto5,
       title: 'Proyecto 5',
-      github: 'https://github.com',
+      github: 'https://github.com/Brogramadores',
       icon: 'bi-file-earmark-word',
     },
     {
       id: 6,
-      img: 'img/proyecto6.webp',
+      img: proyecto6,
       title: 'Proyecto 6',
-      github: 'https://github.com',
+      github: 'https://github.com/Brogramadores',
       icon: 'bi-bootstrap',
     },
   ];
 
   return (
-    <section className="projects-section py-5">
+    <section className={`${styles.projectsSection} py-5`}>
       <div className="container text-center">
-        <h2 className="seccion-titulo texto-negro">Proyectos</h2>
-        <h3 className="seccion-descripcion">Estos son mis proyectos</h3>
+        <h2 className={styles.seccionTitulo}>Proyectos</h2>
+        <h3 className={styles.seccionDescripcion}>Estos son nuestros proyectos</h3>
         <div className="row">
           {projects.map(project => (
             <div className="col-12 col-md-6 col-lg-4 mb-4" key={project.id}>
-              <div className="proyecto position-relative">
+              <div className={`${styles.proyecto} position-relative`}>
                 <img src={project.img} alt={project.title} className="img-fluid" />
                 <div className="overlay d-flex flex-column justify-content-center align-items-center position-absolute top-0 start-0 w-100 h-100">
-                  <p>{project.title}</p>
-                  <div className="iconos-contenedor d-flex gap-3">
+                  <p className={styles.proyectoTitulo}>{project.title}</p>
+                  <div className={`${styles.iconosContenedor} d-flex gap-3`}>
                     <a href={project.github} target="_blank" rel="noopener noreferrer">
                       <i className="bi bi-github fs-3"></i>
                     </a>
@@ -75,8 +81,8 @@ const ProyectosBro = () => {
           ))}
         </div>
 
-        <a href="https://github.com/GitRaFaeLL" target="_blank" rel="noopener noreferrer">
-          <button type="button" className="btn btn-info mt-4">
+        <a href="https://github.com/Brogramadores" target="_blank" rel="noopener noreferrer">
+          <button type="button" className={`${styles.btnInfo} mt-4`}>
             Mas Proyectos <i className="bi bi-arrow-right-circle-fill"></i>
           </button>
         </a>
